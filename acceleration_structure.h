d8c4edb93fa776de7b9fab62e70288fa6c92334d
Nico <muellernico@outlook.com>
2025-01-05 16:05:24 +0100

    avoid copy of leaf by use of reference

--- acceleration_structure.h
+++ acceleration_structure.h
@@ -198,7 +198,7 @@ __device__ void traverseTree(Octree* octree, const ray& r, OctNode* curr_node, O
                 return;
             }
 
-            OctLeaf curr_leaf = octree->leaves[leaf_index];
+            OctLeaf& curr_leaf = octree->leaves[leaf_index];
             for(int j=0; j < curr_leaf.index_count; j++) {
                 if(hit->num_p_hits < MAX_POSSIBLE_HITS) { 
 					hit->possible_hits[hit->num_p_hits++] = curr_leaf.sphere_indices[j];
